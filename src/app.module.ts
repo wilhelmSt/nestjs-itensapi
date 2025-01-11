ee8c43a7462ef42de5ad20929f9bb4e362284867
Wilhelm Steins <wilhelmsteins@gmail.com>
2025-01-11 15:27:30 -0300

    chore: implementing typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+
+// Import the Controllers
 import { AppController } from './app.controller';
+
+// Import the Services
 import { AppService } from './app.service';
+
+// Import the Modules
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
